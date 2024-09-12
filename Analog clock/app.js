cb0ab9d56121f121430a48e7c22a4df44a16f5bc
kronus29 <kronus2919@gmail.com>
2024-09-12 00:04:14 -0500

    first commit

---
+++ Analog clock/app.js
@@ -0,0 +1,58 @@
+var numberHours = document.querySelector('.number_hours')
+var secondsBar = document.querySelector('.seconds_bar')
+
+var numberElement = []
+var barElement = []
+
+for(let i = 1; i<= 12; i++){
+    numberElement.push(
+        `<span style="--index:${i}"><p>${i}</p></span>`
+    )
+
+    console.log(numberElement);
+}
+
+numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""))
+console.log(numberHours)
+
+for(let i = 1; i<= 60; i++){
+    barElement.push(
+        `<span style="--index:${i}"><p></p></span>`
+    )
+}
+
+secondsBar.insertAdjacentHTML("afterbegin", barElement.join(""))
+console.log(numberHours)
+
+
+// Time
+const handHours = document.querySelector('.hand.hour')
+const handMinutes = document.querySelector('.hand.minute')
+const handSeconds = document.querySelector('.hand.second')
+
+function getCurentTime(){
+    let date = new Date()
+    let currentHours = date.getHours()
+    let currentMinutes = date.getMinutes()
+    let currentSeconds = date.getSeconds()
+
+    // 12 hours = 360deg so 1h = 360 / 12 = 30deg  
+    // 1 hour = 30deg that means 60 min = 30deg so 1 min = 30 / 60 = 0.5 or 1/2deg
+    // so formula for hours is (hour * 30 + minutes / 2)
+    handHours.style.transform = `rotate(${currentHours * 30 + currentMinutes / 2}deg)`
+
+    // 60 minutes = 360deg so 1 min = 360 / 60 = 6deg
+    handMinutes.style.transform = `rotate(${currentMinutes * 6}deg)`
+
+    // 60 seconds -= 360deg so 1 sec = 360 /60 = 6deg
+    handSeconds.style.transform = `rotate(${currentSeconds * 6}deg)`
+
+    // sound effect for second hand
+    let sound = new Audio('./sound.wav')
+    sound.play()
+}
+
+getCurentTime()
+
+// call getCurrentTime to set clock hands every second
+setInterval(getCurentTime, 1000) //1000ms = 1s
